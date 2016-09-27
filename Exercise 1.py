928969f43b675a52873168d062cefa261a7e5de0
mariakalfountzou <kalfountzou.maria@gmail.com>
2016-09-27 23:11:14 +0300

    Upload exercise 1

---
+++ Exercise 1.py
@@ -0,0 +1,18 @@
+for i in range (1,10):
+    print("")
+    for j in range (9-i):
+        print(0,end='')
+    for m in range (i):
+        print(i,end='')
+    
+        
+        
+    
+           
+        
+       
+        
+
+
+
+
